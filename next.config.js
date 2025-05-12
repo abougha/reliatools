35e8be7f4c9d4a5694e7473f78a2e6234f6cc325
abougha <ahmed.abougharam@gmail.com>
2025-05-11 22:32:00 -0400

    delete all

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // You can add other options here if needed
-};
-
-module.exports = nextConfig;
