f3e52bf93f7dfb996fbb9ff511ef76164acfcc02
abougha <ahmed.abougharam@gmail.com>
2025-05-24 15:46:26 -0400

    google 2

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,9 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.reliatools.com',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  changefreq: 'weekly',
-  priority: 0.7,
-  exclude: ['/admin', '/api/*'],
+  siteUrl: 'https://www.reliatools.com', // No trailing slash!
+  generateRobotsTxt: true, // Generates robots.txt
+  sitemapSize: 5000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      'https://www.reliatools.com/sitemap.xml',
+    ],
+  },
 };
